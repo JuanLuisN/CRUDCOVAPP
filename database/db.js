ab7b057ef1890a72ad02d99a4476087969f05b85
JuanLuisN <82962748+JuanLuisN@users.noreply.github.com>
2021-05-17 16:22:59 -0500

    Update db.js

--- database/db.js
+++ database/db.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql');
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
+    host: 'b5pofpw3ei4wd0sy1eea-mysql.services.clever-cloud.com',
+    user: 'u5hoog4tmttkkj9h',
+    password: '9bOEXNcsoAueiw8JA0xL',
+    database: 'b5pofpw3ei4wd0sy1eea'
 
 });
 connection.connect((error)=>{
