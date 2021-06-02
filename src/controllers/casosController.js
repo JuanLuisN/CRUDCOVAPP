033f891a6a9fc5b4292e140b35e51e0b8207c505
HectorMu <42363909+HectorMu@users.noreply.github.com>
2021-06-02 18:17:44 -0500

    Update casosController.js

--- src/controllers/casosController.js
+++ src/controllers/casosController.js
@@ -1,23 +1,7 @@
 const controller = {};
 
 controller.listc = (req, res) => 
-{
-controller.search = (req, res)=>{
-  const buscar = req.body.buscar;
-  console.log(buscar)
-  req.getConnection((err, conn)=>{
-      conn.query("select * from casos where fecha like ?",['%'+buscar+'%'],(err, caso)=>{
-          console.log(caso);
-          
-          res.render('casos',{
-              
-              data:caso
-              
-          })
-      })
-  })
-}
-    
+{   
     req.getConnection((err, conn) => 
     {
       conn.query('SELECT * FROM casos ORDER BY IdCasos DESC', (err, casos) => 
